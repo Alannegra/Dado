aa841931956db293732af6e73ac2470198723fc9
Alannegra <alanrodriguez@elpuig.xeill.net>
2021-10-05 17:08:12 +0200

    5/08/2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,32 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
-        Dau dau = new Dau();
+        int caras = 6;
+        int dau1;
+        int dau2;
+
+        int n = scanner.nextInt();
 
 
-        dau.metodoTirar(10);
+            int[] array = new int[caras*2+3];
+
+            for (int i = 0; i < n; i++) {
+                dau1 = (int)(Math.random()*6+1);
+                dau2= (int)(Math.random()*6+1);
+                array[dau1+dau2] ++;
+            }
+            for (int i = 2; i < array.length-2; i++) {
+                System.out.println(i+ "-----> "+ array[i]);
+            }
+
+
+
 
     }
 }
